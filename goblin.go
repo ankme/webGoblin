f3775e60f3aea539f10451b297fcd46086de5eea
ankme <ankme@users.noreply.github.com>
2018-08-16 15:54:14 -0700

    http publishing greenlight
    8080 and /ping working correctly

--- goblin.go
+++ goblin.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+//	"strings"
+)
+
+// simple page writer function
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	//message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	
+	w.Write([]byte(message))
+}
+
+// reply ping response
+func ping(w http.ResponseWriter, r *http.Request) {
+  w.Write([]byte("pong"))
+}
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	http.Handle("/", http.FileServer(http.Dir("../../../src")))
+	http.HandleFunc("/ping", ping)
+	
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
 }
